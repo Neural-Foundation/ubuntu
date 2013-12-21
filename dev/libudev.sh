62afb1d10b0c8681281b41045e3b9f3406501321
徐晨阳 <xuender@gmail.com>
2013-12-21 11:09:22 +0800

    Cannot open shared object file libudev.so.0” error

---
+++ dev/libudev.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# libudev.sh
+# Copyright (C) 2013 ender xu <xuender@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+sudo ln -sf /lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libudev.so.0
+
