ef96c4d123406e1fb674f76266d518383a300472
徐晨阳 <xuender@gmail.com>
2013-12-30 14:59:15 +0800

    print & font

---
+++ install/font.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# font.sh
+# Copyright (C) 2013 ender xu <xuender@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+cd /tmp
+wget http://www.intellij.org.cn/downloads/YaHei.Consolas.1.12.zip
---
+++ install/print.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# print.sh
+# Copyright (C) 2013 ender xu <xuender@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+# 虚拟打印器
+sudo aptitude install cups-pdf 
