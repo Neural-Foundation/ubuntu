0bc1d55dcc0e9eaee3c0b981d8002113c5c83c4f
徐晨阳 <xuender@gmail.com>
2015-05-06 12:52:02 +0800

    obs

---
+++ install/obs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+# obs.sh
+# 录屏软件
+# Copyright (C) 2015 ender xu <xuender@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update && sudo apt-get install obs-studio
