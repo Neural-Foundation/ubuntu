c197fca8d7618784fe470db7f288791a8b64e8ab
徐晨阳 <xuender@gmail.com>
2016-12-25 10:39:19 +0800

    node

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -6,6 +6,8 @@
 # Distributed under terms of the MIT license.
 #
 # ssl开发
-sudo apt-get install openssl
-sudo apt-get install libssl-dev
+sudo apt-get install openssl -f
+sudo apt-get install libssl-dev -f
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+cnpm install -g phantomjs-prebuilt
