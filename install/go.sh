d39eed65358b1f2eaf0c06001381860e3899c019
徐晨阳 <xuender@gmail.com>
2017-04-06 12:39:26 +0800

    修改

--- install/go.sh
+++ install/go.sh
@@ -14,3 +14,4 @@ go get -u github.com/golang/lint/golint
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
+go get github.com/smartystreets/goconvey
