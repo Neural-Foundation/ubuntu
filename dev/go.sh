d4df702c3ad148765a22758c6217ebd409da1b86
徐晨阳 <xuender@gmail.com>
2017-06-15 14:51:08 +0800

    go tagbar

---
+++ dev/go.sh
@@ -0,0 +1,8 @@
+go get github.com/smartystreets/goconvey/convey
+go get github.com/alecthomas/gometalinter
+
+go get github.com/nsf/gocode
+go get code.google.com/p/rog-go/exp/cmd/godef
+go get code.google.com/p/go.tools/cmd/oracle
+go get code.google.com/p/go.tools/cmd/gorename
+go get github.com/jstemmer/gotags
