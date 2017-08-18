119cfd7d5103cce3f0681f3917e11bc0b59c4c89
徐晨阳 <xuender@gmail.com>
2017-08-18 20:34:13 +0800

    负载

--- install/sys.sh
+++ install/sys.sh
@@ -1,9 +1,10 @@
-sudo apt-get install -y aptitude
-# sudo aptitude install -y postgresql pgadmin3 python-pip pylint
-sudo aptitude install -y curl openssl libssl-dev openssh-server
-sudo aptitude install -y build-essential python-dev
-# sudo aptitude install -y postgresql-server-dev-9.1
-# sudo aptitude install -y libcurl4-openssl-dev
+sudo apt install -y apt
+# sudo apt install -y postgresql pgadmin3 python-pip pylint
+sudo apt install -y curl openssl libssl-dev openssh-server
+sudo apt install -y build-essential python-dev
+# sudo apt install -y postgresql-server-dev-9.1
+# sudo apt install -y libcurl4-openssl-dev
 # 系统监控
-sudo aptitude install -y glances
+# sudo apt install -y glances
+sudo apt install indicator-multiload
 
