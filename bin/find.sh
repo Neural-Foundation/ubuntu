25b7869cfc66f56475c10663c1ab5bb8b988baaa
徐晨阳 <xuender@gmail.com>
2017-09-08 14:57:10 +0800

    颜色

--- bin/find.sh
+++ bin/find.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 # 文件内容搜索
 
-echo '查找扩展文件名:'$1
-echo '查找内容:'$2
-find ./ -name '*.'$1 | xargs grep $2 --colour
+
+if [ ! "$1" ] ;then
+	echo "请输入扩展文件名，例如: json"
+fi
+if [ ! "$2" ] ;then
+	echo "请输入查找内容"
+	exit
+fi
+
+echo -e "查找扩展文件名: \033[35m"$1"\033[0m"
+echo -e "查找内容: \033[31m"$2"\033[0m"
+find ./ -name "*."$1 | xargs grep $2 --colour
